edc30b49046751d3e13a1e66009d42ae9250034c
goldylucks <adamgoldman7@gmail.com>
2017-07-05 15:57:16 +0800

    chore(travis): dummy change for trigerring deploy
    heroku said nothing has changed on the last commit

--- src/shared/config.js
+++ src/shared/config.js
@@ -4,4 +4,4 @@ export const WDS_PORT = 7000
 export const APP_CONTAINER_CLASS = 'js-app'
 export const APP_SELECTOR = `.${APP_CONTAINER_CLASS}`
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'GTD APP'
+export const APP_NAME = 'Getting Things Done APP'
